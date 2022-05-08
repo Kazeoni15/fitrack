d9ea63ac6836982e2f483e82a21f64f9a61cc6d0
Kazeoni15 <kazeoni15@gmail.com>
2022-05-08 21:16:19 +0530

    v2.6

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -4,16 +4,17 @@ export default async function handler(req, res) {
   const { method } = req;
 
   if (method == "GET") {
-    res.setHeader(
-      "Set-Cookie",
-      cookie.serialize("jwt", "", {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== "development",
-        expires: new Date(0),
-        sameSite: "strict",
-        path: "/",
-      })
-    );
+    // res.setHeader(
+    //   "Set-Cookie",
+    //   cookie.serialize("jwt", "", {
+    //     httpOnly: true,
+    //     secure: process.env.NODE_ENV !== "development",
+    //     expires: new Date(0),
+    //     sameSite: "strict",
+    //     path: "/",
+    //   })
+    // );
+    res.setHeader("Set-Cookie", cookie.serialize("jwt", "", {httpOnly:true, secure: process.env.NODE_ENV !== "development", expires: new Date(0) ,sameSite: "strict", path:"/"}))
     res.status(200).send("logged out");
   }
 }
--- pages/login.jsx
+++ pages/login.jsx
@@ -39,7 +39,8 @@ export default function LogIn() {
         
         onOpen()
       } else{
-        console.log(res)
+        router.push("/MyPlan")
+        
       }
 
      
