058e9406ec7ad22f8db9e125eef112913c41683b
Kazeoni15 <kazeoni15@gmail.com>
2022-05-08 21:43:19 +0530

    v2.9.1

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -4,17 +4,16 @@ export default async function handler(req, res) {
   const { method } = req;
 
   if (method == "GET") {
-    // res.setHeader(
-    //   "Set-Cookie",
-    //   cookie.serialize("jwt", "", {
-    //     httpOnly: true,
-    //     secure: process.env.NODE_ENV !== "development",
-    //     expires: new Date(0),
-    //     sameSite: "strict",
-    //     path: "/",
-    //   })
-    // );
-   await res.cookie("jwt", "", { expires: new Date(0)})
-   await res.status(200).send("logged out");
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("jwt", "", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+        expires: new Date(0),
+        sameSite: "strict",
+        path: "/api",
+      })
+    );
+    res.status(200).send("logged out");
   }
 }
